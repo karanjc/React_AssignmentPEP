f11232863828b3286984d473e1d5bb8207175642
Tech Wizardry <you@example.com>
2023-02-15 22:27:55 +0530

    1st commit

---
+++ src/pages/PrivacyPolicy.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const PrivacyPolicy = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="container my-3 py-3">
+        <h1 className="text-center">PRIVACY POLICY</h1>
+        <hr />
+        <p className="lead text-center">
+        <b>PRIVACY NOTICE</b><br/>
+
+
+
+Last updated February 11, 2023<br/><br/>
+
+This privacy notice for Beatz4 Tech ("Company" "we," "us," or "our"), describes how and why we might collect, store, use, and/or share ("process") your information when you use our services ("Services"), such as when you:
+
+Visit our website, or any website of ours that links to this privacy notice
+
+Engage with us in other related ways, including any sales, marketing, or events.<br/><br/>
+
+<b>Questions or concerns?</b> Reading this privacy notice will help you understand your privacy rights and choices. If you do not agree with our policies and practices, please do not use our Services. If you still have any questions or concerns, please contact us at karujc25@gmail.com.
+<br/><br/>
+
+<b>SUMMARY OF KEY POINTS</b><br/><br/>
+
+This summary provides key points from our privacy notice, but you can find out more details about any of these topics by clicking the link following each key point or by using our table of contents below to find the section you are looking for. You can also click here to go directly to our table of contents.<br/><br/>
+
+<b>What personal information do we process?</b> When you visit, use, or navigate our Services we may process personal information depending on how you interact with.
+
+Beatz4Tech and the Services, the choices you make, and the products and features you use Click here to learn more.<br/><br/>
+
+<b>Do we process any sensitive personal information?</b> We do not process sensitive personal information.<br/><br/>
+
+<b>Do we receive any information from third parties?</b> We do not receive any information from third parties.<br/><br/>
+
+<b>How do we process your information?</b> We process your information to provide, improve, and administer our Services, communicate with you, for security and fraud prevention, and to comply with law. We may also process your information for other purposes with your consent. We process your information only when we have a vald legal reason to do so. Click here to learn more.<br/><br/>
+
+<b>In what situations and with which parties do we share personal information?</b> We may share information in specific situations and with specific third parties.
+
+Click here to learn more.<br/><br/>
+
+<b>What are your rights?</b> Depending on where you are located geographically, the applicable privacy law may mean you have certain rights regarding your personal
+
+Information. Click here to learn more.<br/><br/>
+
+<b>How do you exercise your rights?</b> The easiest way to exercise your rights is by filling out our data subject request form available here: us. We will consider and act upon any request in accordance with applicable data protection laws.
+
+or by contacting.<br/><br/>
+
+<b>Want to learn more about what Beatz4 Tech does with any information we collect?</b> Click here to review the notice in full.
+        </p>
+
+        
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default PrivacyPolicy
---
+++ src/pages/TermsnConditions.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import { Footer, Navbar } from "../components";
+const TermsnConditions = () => {
+  return (
+    <>
+      <Navbar /><></>
+      <div className="container my-3 py-3"><br/>
+        <h1 className="text-center">TERMS AND CONDITIONS</h1>
+        <hr />
+        <p className="lead text-center">
+        <u><b>1. AGREEMENT TO TERMS:</b></u><br/><br/>
+
+These Terms of Use constitute a legally binding agreement made between you, whether personally or on behalf of an entity ("you") and Beatz4Tech ("Company", "we".
+
+"us", or "our"), concerning your access to and use of the
+
+website as well as any other media form, media channel, mobile website or mobile application
+
+related, linked, or otherwise connected thereto (collectively, the "Site"). We are registered in India and have our registered office at that by accessing the Site, you have read, understood, and agreed to be bound by all of these Terms of Use. <br/><br/>
+<b>IF YOU DO NOT AGREE WITH ALL OF THESE TERMS OF USE, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY.</b><br/><br/> You agree
+
+Supplemental terms and conditions or documents that may be posted on the Site from time to time are hereby expressly Incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Terms of Use at any time and for any reason. We will alert you about any changes by updating the "Last updated" date of these Terms of Use, and you waive any right to receive specific notice of each such change. Please ensure that you check the applicable Terms every time you use our Site so that you understand which Terms apply. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Terms of Use by your continued use of the Site after the date such revised Terms of Use are posted.
+
+The information provided on the Site is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Site from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable.
+
+All users who are minors in the jurisdiction in which they reside (generally under the age of 18) must have the permission of, and be directly supervised by, their parent or guardian to use the Site. If you are a minor, you must have your parent or guardian read and agree to these Terms of Use prior to you using the Site.<br/><br/>
+
+
+<b><u>2. INTELLECTUAL PROPERTY RIGHTS</u></b><br/>
+
+Unless otherwise indicated, the Site is our proprietary property and all source code, databases, functionality, software, website designs, audio, video, text, photographs, and graphics on the Site (collectively, the "Content") and the trademarks, service marks and logos contained therein (the "Marks") are owned or controlled by us or licensed to us, and are protected by copyright and trademark laws and various other intellectual property rights and unfair competition laws of the United States International copyright laws, and international conventions. The Content and the Marks are provided on the Site "AS IS" for your information and personal use only Except as expressly provided in these Terms of Use, no part of the Site and no Content or Marks may be copied, reproduced, aggregated, republished, uploaded, postest publicly displayed, encoded, translated, transmitted, distributed, sold, licensed, or otherwise exploited for any commercial purpose whatsoever, without our express prior wit permission.
+
+Provided that you are eligible to use the Site, you are granted a limited license to access and use the Site and to download or print a copy of any portion of the Content to which you have properly gained access solely for your personal, non-commercial use. We reserve all rights not expressly granted to you in and to the Site, the Content and the Marks.<br/><br/>
+
+<b><u>3. USER REPRESENTATIONS</u></b><br/>
+
+By using the Site, you represent and warrant that: (1) all registration information you submit will be true, accurate, current, and complete:<br/> (2) you will maintain the accuracy of such information and promptly update such registration information as necessary.
+<br/> (3) you have the legal capacity and you agree to comply with these Term of Use; <br/>
+(4) you are not a minor in the jurisdiction in which you reside, or if a minor, you have received parental permission to use the Site;
+<br/> (5) you will not access the Site through automated or non-human means, whether through a bot, script or otherwise: 
+<br/>(6) you will not use the Site for any lilegal or unauthorized purpose; and
+<br/> (7) your use of the Site will not violate any applicable law or regulation.
+
+If you provide any information that is untrue, inaccurate, not current, or incomplete, we have the right to suspend or terminate your account and refuse any and all current or future use of the Site (or any portion thereof).
+        <br/><br/>
+
+        
+       <b><u> 4. USER REGISTRATION:</u></b><br/>
+
+You may be required to register with the Site. You agree to keep your password confidential and will be responsible for all use of your account and password. We reserve the right to remove, reclaim, or change a username you select if we determine, in our sole discretion, that such username is inappropriate, obscene, or otherwise objectionable.<br/><br/>
+<br/>
+<b><u>5. PRODUCTS:</u></b><br/>
+
+We make every effort to display as accurately as possible the colors, features, specifications, and details of the products available on the Site. However, we do not guarantee that the colors, features, specifications, and details of the products will be accurate, complete, reliable, current, or free of other errors and your electronic display may not accurately reflect the actual colors and detalls of the products. All products are subject to availability, and we cannot guarantee that items will be in stock We reserve the right to discontinue any products at any time for any reason. Prices for all products are subject to change.<br/><br/>
+<br/>
+<b><u>6. PURCHASES AND PAYMENT:</u></b><br/>
+
+We accept the following forms of payment:<br/><br/>
+
+- Visa<br/>
+
+- PayPal<br/>
+
+- Mastercard<br/><br/>
+
+You agree to provide current, complete, and accurate purchase and account information for all purchases made via the Site. You further agree to promptly update account and payment information, including email address, payment method, and payment card expiration date, so that we can complete your transactions and contact you as needed. Sales tax will be added to the price of purchases as deemed required by us. We may change prices at any time. All payments shall be in Rupees.</p>
+
+        
+      </div>
+      <Footer />
+    </>
+  )
+}
+
+export default TermsnConditions
